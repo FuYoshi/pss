1b7925b575942b31d6e6820b592d3353246c40e8
Yoshi Fu <yoshifu2002@gmail.com>
2023-09-12 12:14:17 +0200

    hw2

---
+++ homework2.pl
@@ -0,0 +1,90 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem Solving and Search
+% Homework 2
+% Name: ...
+% Group: ...
+% Email: ...
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 1 (sublist)
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 2 (drawing triangles)
+
+% (a) line/2
+
+% ...
+
+% (b) draw/2
+
+% ...
+
+% (c) triangle/1
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 3 (Ex 4.1 (a) from Lecture Notes: plink/plonk)
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 4
+
+% (a) yfy
+
+% ...
+
+% (b) bli bla
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 5 (Ex 3.3 and 3.4 from Lecture Notes: Fibonacci)
+
+% (a) Fibonacci
+
+% ...
+
+% (b) Fast Fibonacci
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 6 (Ex 3.9 from Lecture Notes: People)
+
+born(jan, date(20,3,1977)).
+born(jeroen, date(2,2,1992)).
+born(joris, date(17,3,1995)).
+born(jelle, date(1,1,2004)).
+born(jesus, date(24,12,0)).
+born(joop, date(30,4,1989)).
+born(jannecke, date(17,3,1993)).
+born(jaap, date(16,11,1995)).
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Bonus Question 7 (Ex 3.13 from Lecture Notes: Goldbach)
+
+% ...
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Declaration
+%
+% ... see Canvas "How to submit" for what to write here.
+%
+% Number of LC hours I attended this week: ...
+%
+% Additional hours I spent on the homework: ...
