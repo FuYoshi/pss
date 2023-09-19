4134bc869883ff0ff2b76321b17b88c11dd43924
Yoshi Fu <yoshifu2002@gmail.com>
2023-09-19 12:23:43 +0200

    added hw3 template

---
+++ homework3.pl
@@ -0,0 +1,70 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem Solving and Search
+% Homework 3
+% Name: Yoshi Fu
+% Group: J
+% Email: yoshifu2002@gmail.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 1 (Ex 5.3 from the Lecture Notes: Euclid)
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 2 (Ex 5.4 from the Lecture Notes: occurrences/3)
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 3 (splitting lists of integers)
+
+% (a) separation/3
+
+% ...
+
+% (b) separationNoCut/3
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 4 (Ex 5.7 from Lecture Notes: voting power)
+
+countries([belgium, france, germany, italy, luxembourg, netherlands]).
+
+weight(france, 4).
+weight(germany, 4).
+weight(italy, 4).
+weight(belgium, 2).
+weight(netherlands, 2).
+weight(luxembourg, 1).
+
+threshold(12).
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Question 5 (dynamic counter)
+
+% ...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Bonus Question 6 (robot navigation)
+
+% ...
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Declaration
+%
+% I hereby declare that these solutions are entirely my own work and no part has been copied from other sources.
+%
+% Number of LC hours I attended this week: 3
+%
+% Additional hours I spent on the homework: 6
