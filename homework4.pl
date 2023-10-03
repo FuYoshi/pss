371e908d80670ec96419da76b1a728702a762047
Yoshi Fu <yoshifu2002@gmail.com>
2023-10-03 17:39:48 +0200

    hw4

--- homework4.pl
+++ homework4.pl
@@ -19,14 +19,33 @@
 % Question 1: insert/3                                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+/**
+ * Insert an item into a sorted list and keep it sorted.
+ */
+% Just add the item if the sorted list is empty.
+insert(Item, [], [Item]) :- !.
+% If Item < Head, then it is sorted.
+insert(Item, [Head | Tail], [Item, Head | Tail]) :-
+    Item < Head, !.
+% Otherwise, continue through the tail to find the correct place.
+insert(Item, [Head | Tail], [Head | Result]) :-
+    insert(Item, Tail, Result).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Question 2: insertsort/2                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+/**
+ * Sort a list by sorting the tail and then adding an item to
+ * the sorted tail.
+ */
+% An empty list is already sorted and is a sublist of any other list.
+insertsort([], []).
+% Any list can be sorted by sorting the tail and adding the head with insert/3.
+insertsort([Head | Tail], Result) :-
+    insertsort(Tail, SortedTail),
+    insert(Head, SortedTail, Result).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -48,28 +67,143 @@ grid([ [w, w, w, b, w],
 % Question 3: white/1                                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+/**
+ * Check if a square is white in the grid.
+ */
+white(X/Y) :-
+    grid(Grid),  % Get the grid.
+    nth1(Y, Grid, Row),  % Get the yth row in the grid.
+    nth1(X, Row, Cell),  % Get the xth cell in the row.
+    Cell = w.  % Check if the cell is white.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Question 4: move/2                                        %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+/**
+ * Check the possible valid moves.
+ * A player can only move 1 space left, right, down or up.
+ * For all cases, we compute the new coordinates and check if it is white.
+ */
+move(X/Y, NextState) :-
+    NewX is X-1,
+    NextState = NewX/Y,
+    white(NextState).
+move(X/Y, NextState) :-
+    NewX is X+1,
+    NextState = NewX/Y,
+    white(NextState).
+move(X/Y, NextState) :-
+    NewY is Y-1,
+    NextState = X/NewY,
+    white(NextState).
+move(X/Y, NextState) :-
+    NewY is Y+1,
+    NextState = X/NewY,
+    white(NextState).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Question 5: goal/1                                        %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+/**
+ * Check if coordinate X/Y is the goal in the grid.
+ *
+ * The goal is in the bottom right corner, which is the last row and the last
+ * column.
+ */
+goal(X/Y) :-
+    grid([Head | Tail]),
+    length([Head | Tail], Y),
+    length(Head, X).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Question 6: comparing DFS algorithms                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ...
+depthfirst(Node, []) :-
+    goal(Node).
+    depthfirst(Node, [NextNode|Path]) :-
+    move(Node, NextNode),
+    depthfirst(NextNode, Path).
+
+solve_depthfirst(Node, [Node|Path]) :-
+    depthfirst(Node, Path).
+
+move_cyclefree(Visited, Node, NextNode) :-
+    move(Node, NextNode),
+    \+ member(NextNode, Visited).
+
+depthfirst_cyclefree(Visited, Node, Visited) :-
+    goal(Node).
+
+depthfirst_cyclefree(Visited, Node, Path) :-
+    move_cyclefree(Visited, Node, NextNode),
+    depthfirst_cyclefree([NextNode|Visited], NextNode, Path).
+
+solve_depthfirst_cyclefree(Node, Path) :-
+    depthfirst_cyclefree([Node], Node, RevPath),
+    reverse(RevPath, Path).
+
+depthfirst_bound(_, Visited, Node, Visited) :-
+    goal(Node).
+
+depthfirst_bound(Bound, Visited, Node, Path) :-
+    Bound > 0,
+    move_cyclefree(Visited, Node, NextNode),
+    NewBound is Bound - 1,
+    depthfirst_bound(NewBound, [NextNode|Visited], NextNode, Path).
+
+solve_depthfirst_bound(Bound, Node, Path) :-
+    depthfirst_bound(Bound, [Node], Node, RevPath),
+    reverse(RevPath, Path).
+
+/*
+plain depthfirst example
+?- solve_depthfirst(1/1, Path).
+Error: Stack limit (1.0Gb) exceeded
+...  % remainder of error message skipped.
+
+The DFS algorithm does not work due to an infinite loop. The algorithms's first
+move is to move to the right (1/1 -> 1/2). Since move/2 checks for the cell on
+the left first, the move after walks to the left again (1/2 -> 1/1). This means
+that the algorithm is caught in a infinite loop with the path:
+[1/1, 1/2, 1/1, 1/2, ...].
+
+?- solve_depthfirst_cyclefree(1/1, Path).
+Path = [1/1, 2/1, 3/1, 3/2, 3/3, 2/3, 1/3, 1/4, 1/5, 2/5, 3/5, 4/5, 5/5] ;
+false.
+
+The DFS cyclefree algorithm finds all paths that reach the goal and do not
+contain any cycles. This can solve the maze as arriving at the same cell again
+does not give you new options to move to. Therefore, removing the cycles only
+removes irrelevent paths.
+
+?- solve_depthfirst_bound(12, 1/1, Path).
+Path = [1/1, 2/1, 3/1, 3/2, 3/3, 2/3, 1/3, 1/4, 1/5, 2/5, 3/5, 4/5, 5/5] ;
+false.
+
+The DFS bound algorithm finds all paths that have a length of Bound and reach
+the goal. Since the Bound used is greater than or equal to 12 a path could be
+found, 12 being the length of the shortest path from start to goal in this
+particular maze.
+
+? Write down which algorithm you recommend for this problem, and why.
+Either the DFS cyclefree algorithm or DFS bound algorithm should be used.
+This is because both algorithms do not get stuck in a infinite loop inside the
+maze. If the user wants to find all possible solutions, then DFS cyclefree
+should be used. For bigger mazes, however, the shortest may only consist of a
+very small portion of the entire maze. In this case, using the DFS bound
+algorithm could ignore the long paths that may exist saving some computing
+time. The problem with the DFS bound algorithm is that if the bound is chosen
+too small, the algorithm will not find any solution. Therefore, I would
+recommend using the DFS cyclefree algorithm and only recommend using the
+DFS bound algorithm when more specified information about the maze is available
+(such as shortest path length).
+*/
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -77,6 +211,60 @@ grid([ [w, w, w, b, w],
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % ...
+
+/**
+ * Replace an element at index with value. The index counter starts at 1.
+ *
+ * The element at index Index is found by getting the head of the list and
+ * decremening Index. When Index = 1, the element at Index is found.
+ */
+% Replace the first element with Value.
+replace1([_ | List], 1, Value, [Value | List]) :- !.
+% Otherwise, iterate through list until and decrement Index until Index = 1.
+replace1([Head | List], Index, Value, [Head | NewList]) :-
+    NextIndex is Index - 1,
+    replace1(List, NextIndex, Value, NewList).
+
+/**
+ * Edit the cell with coordinates X/Y in the Grid to be Value set the result
+ * to NewGrid.
+ */
+editgrid(Grid, X/Y, Value, NewGrid) :-
+    nth1(Y, Grid, Row),  % Find the row of the cell to replace.
+    replace1(Row, X, Value, NewRow),  % Replace the row with its new cell at index.
+    replace1(Grid, Y, NewRow, NewGrid).  % Replace the grid with its new row at index.
+
+/**
+ * Write the grid to stdout in a way that is readable for humans.
+ */
+writegrid([]).
+writegrid([Head | Tail]) :-
+    write(Head),
+    nl,
+    writegrid(Tail).
+
+/**
+ * Repeat a certain action Goal N amount of times.
+ */
+repeat(_, 0) :- !.
+repeat(Goal, N) :-
+    call(Goal),
+    N1 is N - 1,
+    repeat(Goal, N1).
+
+/**
+ * Animate a Path taken through the maze by replacing the cell in the grid at
+ * X/Y with p (for Player). Write this new grid to stdout and after sleeping
+ * for a bit, write the next move.
+ */
+animate([]) :- !.
+animate([Head | Tail]) :-
+    grid(Grid),
+    editgrid(Grid, Head, p, PlayerGrid),  % Edit Grid to contain the player.
+    repeat(nl, 30),  % Wipe the board.
+    writegrid(PlayerGrid),  % Show a frame of the animation.
+    sleep(1),  % Wait a bit so humans can see the frame.
+    animate(Tail).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Declaration
